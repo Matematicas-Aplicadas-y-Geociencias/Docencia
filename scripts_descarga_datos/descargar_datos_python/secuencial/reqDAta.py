99398d71d21d0a6526a062d935d165d00c5692ec
Edwin Perez <edwin.perez@enesmerida.unam.mx>
2026-03-26 09:42:25 -0600

    chore: rutina para la clase de computacion y analisis de datos geofisicos

---
+++ scripts_descarga_datos/descargar_datos_python/secuencial/reqDAta.py
@@ -0,0 +1,16 @@
+import requests
+
+year = 2002
+day = 214
+hour = 12
+
+filename: str = f"010_archv.{year}_{day}_{hour:02d}_2d.nc"
+
+url: str = f"https://tds.hycom.org/thredds/fileServer/datasets/GOMb0.01/reanalysis/data/{year}/{filename}"
+
+response = requests.get(url)
+
+response.raise_for_status()
+
+with open(filename, "wb") as f:
+    f.write(response.content)
